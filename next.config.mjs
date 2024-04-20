7e553435c782b9f6da50b4fb1480a8905539278c
ErikColacios <ecolaciosgarcia@gmail.com>
2024-04-20 12:35:27 +0200

    base url changed

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
                 source: "/app/(.*)",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: process.env.BASE_URL }, // replace this your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Authorization, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
