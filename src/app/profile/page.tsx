4dd641a39e90da614cd7d63870290ddb78af205e
ErikColacios <ecolaciosgarcia@gmail.com>
2024-07-25 21:46:01 +0200

    profile page responsive fixes

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -33,13 +33,13 @@ export default async function Profile() {
        
 
     return (
-        <section className="flex flex-col md:flex-row space-y-12 md:space-x-12 w-full h-screen pt-24 p-6 md:p-12 text-white bg-[url('/staticImages/dark_bg.jpg')] bg-cover">
-            <div className="flex flex-col md:w-1/3 shadow-lg bg-zinc-900/80 greenShadow">
+        <section className="flex flex-col lg:flex-row space-y-12 lg:space-x-12 w-full h-screen pt-24 p-6 md:p-12 text-white bg-[url('/staticImages/dark_bg.jpg')] bg-cover">
+            <div className="flex flex-col lg:w-1/2 xl:w-1/3 shadow-lg bg-zinc-900/80 md:mt-12 greenShadow">
 
                 {/* DIV PROFILE IMAGE */}
                 <div className="relative h-70 z-20">
                     <img src="/staticImages/eldenring.jpg" />
-                    <div className="w-36 h-36 2xl:w-48 2xl:h-48 rounded-full overflow-hidden ml-6 absolute top-8 2xl:top-14">
+                    <div className="w-36 h-36 2xl:w-48 2xl:h-48 rounded-full overflow-hidden ml-6 absolute top-8 sm:top-24 md:top-32 lg:top-14">
                         <img src="/profileImages/riku.jpg" className="h-full w-full object-cover"/>
                     </div>
                 </div>
@@ -47,31 +47,48 @@ export default async function Profile() {
                 <div className="p-6 pt-16">
                     <div className="flex items-center relative">
                         <p className="text-4xl font-bold">{user_name}</p>
-                        <button className="bg-black border border-[#00FF11] pl-10 p-2 pr-10 absolute right-0 hover:bg-[#00FF11] hover:text-black">EDIT PROFILE</button>
+                        <button className="bg-black border border-[#00FF11] pl-6 pr-6  sm:pl-10 p-2 sm:pr-10 absolute right-0 hover:bg-[#00FF11] hover:text-black">EDIT PROFILE</button>
                     </div>
                     {userinfo.map((item:any, index:number)=>(
                     <div className="mt-4" key={index}>
                         <p className="text-xl mb-1">Biography</p>
                         <p>{item.user_bio}</p>
-                        <div className="mt-2">
-                            <span className="text-xl mr-12">Joined</span><span>{item.user_creationdate}</span>
+
+                        <div className="flex mt-8 ">
+                            <div className="text-md sm:text-lg mr-8 text-green-400">
+                                <p>Joined</p>
+                                <p>Email</p>
+                                <p>Location</p>
+                                <p>Webpage</p>
+                            </div>
+                            <div className="text-md sm:text-lg">
+                                <p className="">{item.user_creationdate}</p>
+                                <p className="">{item.user_email}</p>
+                                <p className="">{item.user_location}</p>
+                                <a className="underline" href={"https://"+item.user_webpage} target="_blank" rel="noopener noreferrer">{item.user_webpage}</a>
+                            </div>
                         </div>
-                        <div className="mt-2">
-                            <span className="text-xl mr-12">Email</span><span>{item.user_email}</span>
-                        </div>
-                        <div className="mt-2">
-                            <span className="text-xl mr-12">Location</span><span>{item.user_location}</span>
-                        </div>
-                        <div className="mt-2">
-                            <span className="text-xl mr-12">Webpage</span><a className="underline" href={"https://"+item.user_webpage} target="_blank" rel="noopener noreferrer">{item.user_webpage}</a>
-                        </div>
+                        
+                        {/* <div className="mt-4">
+                                <span className="text-xl mr-12">Joined</span><span>{item.user_creationdate}</span>
+                            </div>
+                            <div className="mt-2">
+                                <span className="text-xl mr-12">Email</span><span>{item.user_email}</span>
+                            </div>
+                            <div className="mt-2">
+                                <span className="text-xl mr-12">Location</span><span>{item.user_location}</span>
+                            </div>
+                            <div className="mt-2">
+                                <span className="text-xl mr-12">Webpage</span><a className="underline" href={"https://"+item.user_webpage} target="_blank" rel="noopener noreferrer">{item.user_webpage}</a>
+                            </div> */}
                     </div>
                     ))}
                 </div>
 
             </div>
 
-            <div className="flex flex-col md:w-2/3 p-12 shadow-lg bg-zinc-900/80 greenShadow">
+            <div className="flex flex-col lg:w-1/2 xl:w-2/3 p-12 shadow-lg bg-zinc-900/80 greenShadow">
+            <p className="text-2xl mb-12">Game stats</p>
                         Coming soon ...
             </div>
 
