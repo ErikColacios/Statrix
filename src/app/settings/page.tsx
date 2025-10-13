66ed4537a25cc520b44148715e70c2fe5bc50b7f
ErikColacios <ecolaciosgarcia@gmail.com>
2025-10-13 18:58:27 +0200

    Settings page updated 0.2

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -41,25 +41,25 @@ export default function Settings(){
                         <ChooseAvatarBanner chooseMode={chooseMode} current_avatar_id={item.avatar_image_id} current_banner_id={item.banner_image_id} />
                 </Dialog.Content>
             </Dialog.Portal>
-            <div className="w-full md:w-3/5 2xl:w-2/5 flex flex-col bg-gray-800 bg-zinc-900/80">
+            <div className="w-full md:w-4/5 lg:w-3/5 2xl:w-2/5 flex flex-col bg-gray-800 bg-zinc-900/80">
                 <form className="w-full relative rounded  outline-gray-700" action={formAction}>
 
                         {/* BANNER */}
                         <Dialog.Trigger asChild onClick={() => setChooseMode("banner")}>
                             <div className="w-full">
-                                <img src={"/bannerImages/"+item.banner_image} className="w-full h-56 outline outline-1 outline-gray-700 hover:outline-green-600 cursor-pointer" />
+                                <img src={"/bannerImages/"+item.banner_image} className="w-full h-42 md:h-56 outline outline-1 outline-gray-700 hover:outline-green-600 cursor-pointer" />
                             </div>
                         </Dialog.Trigger>
         
         
                         {/* AVATAR */}
                         <Dialog.Trigger asChild onClick={() => setChooseMode("avatar")}>
-                            <div className="ml-2 absolute top-10 w-48 h-48 rounded-full overflow-hidden outline outline-1 outline-gray-700 hover:outline-green-600 cursor-pointer">
+                            <div className="ml-2 absolute top-10 w-32 h-32 sm:w-48 sm:h-48 rounded-full overflow-hidden outline outline-1 outline-gray-700 hover:outline-green-600 cursor-pointer">
                                 <img src={"/avatarImages/" + item.avatar_image} className="h-full w-full object-cover" />
                             </div>
                         </Dialog.Trigger>
                         
-                            <div className="w-full px-4 py-8">
+                            <div className="w-full px-4 pt-16 sm:pt-8">
                                 <div>
                                     <p className="text-sm text-gray-400">Username</p>
                                     <input type="text" name="user_name" maxLength={20} className="w-full p-1 bg-gray-800 outline-none border border-1 border-gray-700 focus:border-green-600" defaultValue={item.user_name} />
