172c171d675e6eaa1acba66cee35914a9642c7ba
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-06 12:35:01 +0200

    Trying to fix psql connection for the web p4.2

--- src/actions/logInUser.ts
+++ src/actions/logInUser.ts
@@ -1,36 +1,42 @@
-"use server"
-import { pool } from '@/util/postgres';
-import bcrypt from 'bcryptjs';
+"use server";
+import { pool } from "@/util/postgres";
+import bcrypt from "bcryptjs";
 
-export async function logInUser(userName:string, password:string) {
+export async function logInUser(userName: string, password: string) {
+  try {
+    console.log(pool);
 
-    console.log(pool)
-    
     const res = await pool.query(
-        `SELECT user_id, user_name, user_password FROM users WHERE user_name = $1`,
-        [userName]
-    )
+      `SELECT user_id, user_name, user_password FROM users WHERE user_name = $1`,
+      [userName],
+    );
 
     if (res.rows.length === 0) {
-        return { error: "No user found with that user name" };
-    }
-    else if (res.rows.length !== 1) {
-        return { error: "Wrong credentials" };
+      return { error: "No user found with that user name" };
+    } else if (res.rows.length !== 1) {
+      return { error: "Wrong credentials" };
     }
 
-    console.log(res.rows[0].user_password)
+    console.log(res.rows[0].user_password);
 
     if (res.rows[0].user_password === null) {
-        return { error: "Something went wrong" };
+      return { error: "Something went wrong" };
     }
-    
-    const userIdLogged = res.rows[0].user_id
-    const userNameLogged = res.rows[0].user_name
-    const passwordMatch = await bcrypt.compare(password, res.rows[0].user_password);
+
+    const userIdLogged = res.rows[0].user_id;
+    const userNameLogged = res.rows[0].user_name;
+    const passwordMatch = await bcrypt.compare(
+      password,
+      res.rows[0].user_password,
+    );
 
     if (!passwordMatch) {
-        return { error: "Wrong credentials" };
+      return { error: "Wrong credentials" };
     } else {
-        return {userIdLogged, userNameLogged, passwordMatch}
+      return { userIdLogged, userNameLogged, passwordMatch };
     }
+    
+  } catch (error) {
+    console.log("Error in logInUser: ", error);
+  }
 }
