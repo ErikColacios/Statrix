616838d0c838061de8087383c11ecfea4602f1c2
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-07 09:11:37 +0200

    More debugging

--- src/util/postgres.js
+++ src/util/postgres.js
@@ -2,12 +2,19 @@ import { Pool } from 'pg';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export const pool = new Pool({
-  //connectionString: process.env.POSTGRES_URL,
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: process.env.POSTGRES_PORT,
-  //ssl: {rejectUnauthorized: false},
-});
+export const pool = new Pool(
+  console.log({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DATABASE,
+  }),
+  {
+    //connectionString: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT,
+    //ssl: {rejectUnauthorized: false},
+  });
