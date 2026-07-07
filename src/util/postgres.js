9267553783e9646aba6f62b3f098031da6841e89
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-07 09:27:25 +0200

    More debugging

--- src/util/postgres.js
+++ src/util/postgres.js
@@ -2,13 +2,6 @@ import { Pool } from 'pg';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-console.log({
-  host: pool.options.host,
-  port: pool.options.port,
-  database: pool.options.database,
-  user: pool.options.user,
-})
-
 export const pool = new Pool(
   {
     //connectionString: process.env.POSTGRES_URL,
