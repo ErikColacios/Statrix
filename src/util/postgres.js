fd7e19d9a60bb532ad9fde909203c14bf3883063
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-07 13:57:58 +0200

    Connection to postgres now working again

--- src/util/postgres.js
+++ src/util/postgres.js
@@ -1,19 +1,14 @@
-import { Pool } from "pg";
+import { Pool } from 'pg';
 
-console.log("POSTGRES_HOST =", process.env.POSTGRES_HOST);
+const isProduction = process.env.NODE_ENV === 'production';
 
-export const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
-  port: Number(process.env.POSTGRES_PORT),
-});
-
-pool.on("connect", () => {
-  console.log("✅ PostgreSQL conectado");
-});
-
-pool.on("error", (err) => {
-  console.error("❌ Error del pool:", err);
-});
+export const pool = new Pool(
+  {
+    //connectionString: process.env.POSTGRES_URL,
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    port: process.env.POSTGRES_PORT,
+    //ssl: {rejectUnauthorized: false},
+  });
