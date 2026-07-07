f77d5cb0bf1bdd044a104de4647b1dee106e5d45
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-07 09:47:53 +0200

    More debugging

--- src/actions/logInUser.ts
+++ src/actions/logInUser.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 
 export async function logInUser(userName: string, password: string) {
   try {
+    console.log(pool);
     const res = await pool.query(
       `SELECT user_id, user_name, user_password FROM users WHERE user_name = $1`,
       [userName],
