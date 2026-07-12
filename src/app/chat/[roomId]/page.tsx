f49274be24fffd8711d29df236c7eececb9cdab1
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-12 20:37:44 +0200

    Chat fixes

--- src/app/chat/[roomId]/page.tsx
+++ src/app/chat/[roomId]/page.tsx
@@ -10,7 +10,7 @@ import React, { useState, useEffect, FormEvent, useRef, useCallback } from "reac
 import { Socket, io } from "socket.io-client";
 
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  "http://localhost:4000",
+  "https://statrix.app:4000",
 );
 
 export default function Chat({ params }: { params: { roomId: string } }) {
