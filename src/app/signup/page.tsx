2ed5e4d8829bb50bf27b10c265367bd59db4022c
ErikColacios <ecolaciosgarcia@gmail.com>
2026-07-23 14:52:31 +0200

    Sign up and verification updates

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -58,7 +58,7 @@ export default function SignUp() {
                         </>
                     }
                     {(nextSlide === 1) &&
-                        <div className='flex flex-col items-center justify-center h-96 animate-slide-left'>
+                        <div className='flex flex-col items-center justify-center text-center h-96 animate-slide-left'>
                             <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M2 12C2 8.22876 2 6.34315 3.17157 5.17157C4.34315 4 6.22876 4 10 4H14C17.7712 4 19.6569 4 20.8284 5.17157C22 6.34315 22 8.22876 22 12C22 15.7712 22 17.6569 20.8284 18.8284C19.6569 20 17.7712 20 14 20H10C6.22876 20 4.34315 20 3.17157 18.8284C2 17.6569 2 15.7712 2 12Z" stroke="#ffffff" strokeWidth="1.5"></path> <path d="M6 8L8.1589 9.79908C9.99553 11.3296 10.9139 12.0949 12 12.0949C13.0861 12.0949 14.0045 11.3296 15.8411 9.79908L18 8" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path> </g></svg>
                             <p className='text-2xl font-bold mt-2'>Check your email</p>
                             <p className='text-gray-400'>We sent a verification link to {email}</p>
