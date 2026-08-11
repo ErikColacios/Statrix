dffedb9a204ea29fe0c6baea0eedda3b2be61635
ErikColacios <ecolaciosgarcia@gmail.com>
2026-08-11 11:18:05 +0200

    BrowseGames and NewList games gap adjustment

--- src/app/browseGames/page.tsx
+++ src/app/browseGames/page.tsx
@@ -114,7 +114,7 @@ export default function BrowseGames() {
 
           {/* Games shown */}
           {isLoading ? <SkeletonBrowseGames /> :
-            <div className='grid justify-center grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7 gap-8'>
+            <div className='grid justify-center grid-cols-3 md:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 3xl:grid-cols-7 gap-8'>
               {gameItems.map((game, index: number) => (
                 <Dialog.Trigger key={index} onClick={()=> setGameClicked(game)} className={`group relative flex justify-center items-center rounded-2xl overflow-hidden cursor-pointer lg:w-48 lg:h-64 transition hover:scale-110`} >
                     <img src={`https://images.igdb.com/igdb/image/upload/t_720p/${game.cover.image_id}.png`} className='w-full h-full transition duration-300 group-hover:blur-xs group-hover:brightness-50' width={80} height={80} alt='Game cover' />
